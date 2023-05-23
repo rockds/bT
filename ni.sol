8782a3ab00ab6979edd5168be835d60cf1d7f7c8
rockds <134147604+rockds@users.noreply.github.com>
2023-05-23 23:17:37 +0530

    Create ni.sol

---
+++ ni.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+// Creating a contract
+contract shreyansh_05
+{
+ // Defining a function
+ function get_output() public pure returns (string memory){
+ return ("Hi, your contract ran successfully");
+ }
+}
